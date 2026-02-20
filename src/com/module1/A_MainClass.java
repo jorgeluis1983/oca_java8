97a1cd19bdcfcd713349f5750088e6a671d3e19a
Jorge <jlromero06@gmail.com>
2026-02-19 22:25:34 -0500

    add print

--- src/com/module1/A_MainClass.java
+++ src/com/module1/A_MainClass.java
@@ -4,7 +4,7 @@ public class A_MainClass {
 
     public static void main(String[] args){
         System.out.println("fecha"+System.currentTimeMillis());
-
+        System.out.println("fecha2"+System.currentTimeMillis());
 
     }
 
